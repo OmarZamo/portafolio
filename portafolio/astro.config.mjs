5cfa1472a372e75575f1c82a01a967bbb9013046
Omar Zamorano <superomarz13@gmail.com>
2025-06-12 17:06:43 -0600

    primera version del portafolio

--- portafolio/astro.config.mjs
+++ portafolio/astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@cv': path.resolve('./cv.json'), // Alias para que @cv apunte al archivo cv.json
+      },
+    },
+  },
+});
+
